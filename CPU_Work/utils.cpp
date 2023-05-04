f3a88ff4b37b7be8298ee885984d281309ca76fc
Diogo Calado <diogo.calado@siemens.com>
2023-05-04 09:11:17 +0100

    work from 05/03

--- CPU_Work/utils.cpp
+++ CPU_Work/utils.cpp
@@ -10,6 +10,32 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+# include "../Headers/Webserver.hpp"
 
 // need to do here a function that converts binary to hexa.
 // min 5:50 of 2nd video.
+
+char	*binary_to_hex(SA *SAdd)
+{
+	int		i = 0;
+    int     addr = (int)SAdd->sin_port;
+    char    hex_chars[16] = "0123456789ABCDEF";
+	char	*str;
+    while (addr > 0)
+    {
+		str = realloc(str, i);
+        str[i] = hex_chars[addr % 16];
+		addr /= 16;
+		i++;
+    }
+}
+
+char	*realloc(char *str, int len)
+{
+	char *new_str;
+
+	if (str)
+		free(str);
+	new_str = malloc((len + 1) * sizeof(char));
+	return(new_str);
+}
